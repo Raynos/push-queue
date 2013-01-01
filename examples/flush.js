dee92c40be9e67f914cd1d9e8b0b5f55928cb78d
Raynos <raynos2@gmail.com>
2012-12-31 16:19:02 -0800

    cleaned up examples and tests

--- examples/flush.js
+++ examples/flush.js
@@ -1,15 +1,16 @@
 var assert = require("assert")
 var setTimeout = require("timers").setTimeout
+var now = Date.now()
 
 var Queue = require("../index")
 var count = 0
 
 var enqueue = Queue(function (item, callback) {
     console.log("doing", item)
-    setTimeout(function () {
-        console.log("finished", item)
+    process.nextTick(function () {
+        console.log("finished", item, Date.now() - now)
         callback()
-    }, 100)
+    })
 })
 
 enqueue("one", log("flushed one"))
@@ -24,6 +25,6 @@ function log(msg) {
 }
 
 setTimeout(function () {
-    console.log("count", count)
+    console.log("count", count, Date.now() - now)
     assert.equal(count, 3)
-}, 500)
+}, 400)
--- test/index.js
+++ test/index.js
@@ -1,3 +1,12 @@
+var test = require("tape")
+var setTimeout = require("timers").setTimeout
+
 require("./test")
 require("../examples/flush")
 require("../examples/simple")
+
+test("wait", function (assert) {
+    setTimeout(function () {
+        assert.end()
+    }, 460)
+})
